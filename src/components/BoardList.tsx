f3398b97165b196380cff74a5b8fd24193388983
Jaemin Jeon <wjswoals777@icloud.com>
2021-07-18 01:56:04 +0900

    Feat create board
    보드 추가 기능 구현

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -3,13 +3,31 @@ import '../styles/board-list.scss';
 // Element
 import { BoardName } from './elements';
 
+// Reudx, Action
+import { useDispatch, useSelector } from 'react-redux';
+import { boardActions } from '../redux/modules/board';
+import { RootState } from '../redux/modules';
+
 const BoardList = () => {
+  const dispatch = useDispatch();
+  // 보드 정보 가져오기
+  const { list } = useSelector((state: RootState) => state.board);
+
   return (
     <div className="boardlist">
-      <BoardName />
+      {/* 보드 리스트 출력 */}
+      {list.map((board) => {
+        return <BoardName boardId={board.id} name={board.name} key={board.id} />;
+      })}
       {/* 포스트 추가 버튼 */}
       <div className="boardlist-button-wrap">
-        <button>+</button>
+        <button
+          onClick={() => {
+            dispatch(boardActions.createBoard());
+          }}
+        >
+          +
+        </button>
       </div>
     </div>
   );
--- src/components/elements/BoardName.tsx
+++ src/components/elements/BoardName.tsx
@@ -1,9 +1,13 @@
 import '../../styles/board-name.scss';
 
-const BoardName = () => {
+const BoardName = (props: { boardId: number; name: string }) => {
+  const { boardId, name } = props;
   return (
     <div className="boardname">
-      <span>BoardName</span>
+      <span>
+        {/* id와 boardName 표시 */}
+        {boardId}. {name}
+      </span>
     </div>
   );
 };
